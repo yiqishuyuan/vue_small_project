5e5d9c484bd4fdc48aa287b97a1fe9bc4fd3e289
lily <falaonline@outlook.com>
2024-06-09 13:46:01 +0800

    跨组件副本

---
+++ 8.vue-demo-src copy/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="App">
+    <One></One>
+    <Two></Two>
+  </div>
+</template>
+
+<script>
+import One from './components/One.vue';
+import Two from './components/Two.vue';
+
+export default {
+  components: {
+    One,
+    Two
+  }
+}
+</script>
+
+<style></style>
